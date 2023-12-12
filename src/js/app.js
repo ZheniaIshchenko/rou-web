71b79b623ceeed22936270e2a85c4e02c080e86d
ZheniaIshchenko <fydex1@gmail.com>
2023-12-12 16:12:19 +0200

    Delete src directory

--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-import * as flsFunctions from './modules/functions.js';
-import {burger} from './modules/burger.js';
-import {headerShadow} from './modules/headerShadow.js';
-import {scroll} from './modules/scrollAnimation.js';
-flsFunctions.isWebP();
--- src/js/modules/headerShadow.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if(window.scrollY > 0){
-        document.querySelector('.header-section').classList.add('shadow')
-    } else {
-        document.querySelector('.header-section').classList.remove('shadow')
-    }
-});
